e6cf0b347cb3c0b3567a266b5b21d955b0e009b3
maciekdt <maciek.fi88@gmail.com>
2022-01-13 00:34:02 +0100

    fitness

--- GAIndividual.cpp
+++ GAIndividual.cpp
@@ -1,27 +1,53 @@
 #include "GAIndividual.h"
 #include <set>
 
+GAIndividual::GAIndividual(){
+	genotype = new vector<LogicVariable*>();
+}
+
 GAIndividual* GAIndividual::mutation(float probability){
-	for (int i = 0; i < genotype.size(); i++) {
+	GAIndividual* mutatedInd = new GAIndividual();
+	for (int i = 0; i < genotype->size(); i++) {
 		float random = ((float)rand()) / (float)RAND_MAX;
 		if (random < probability) {
-			genotype[i].sign = (!genotype[i].sign);
+			mutatedInd->genotype->push_back(new LogicVariable(genotype->at(i)->id, !(genotype->at(i)->sign)));
 		}
+		else {
+			mutatedInd->genotype->push_back(new LogicVariable(genotype->at(i)->id, genotype->at(i)->sign));
+		}
+	}
+	return mutatedInd;
+}
+
+GAIndividual* GAIndividual::crossOver(GAIndividual* other){
+	GAIndividual* child = new GAIndividual();
+	set<int> set;
+	for (int i = 0; i < genotype->size(); i++) {
+		if (0 + (rand() % (1 - 0 + 1)) == 1) {
+			child->genotype->push_back(new LogicVariable(genotype->at(i)->id, genotype->at(i)->sign));
+		}
+		else {
+			child->genotype->push_back(new LogicVariable(other->genotype->at(i)->id, other->genotype->at(i)->sign));
+		}
+	}
+	return child;
+}
+
+float GAIndividual::fitness(vector<Clause*>* clauses){
+	int numberFitedClauses = 0;
+	for (int i = 0; i < clauses->size(); i++) {
+		bool 
 	}
 }
 
-GAIndividual* GAIndividual::crossOver(GAIndividual* otherParent){
-	GAIndividual* child = new GAIndividual();
-	set<int> set;
-	for (int i = 0; i < genotype.size(); i++) {
-		if (0 + (rand() % (1 - 0 + 1)) == 1) {
-			child->genotype[i] = genotype[i];
-		}
-		else {
-			child->genotype[i] = otherParent->genotype[i];
-		}
+LogicVariable* GAIndividual::getLogicVariableById(int id){
+	for (int i = 0; i < genotype->size(); i++) {
+		if (genotype->at(i)->id == id) return genotype->at(i);
 	}
+	return nullptr;
 }
+
+
 
 
 
--- GAIndividual.h
+++ GAIndividual.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <vector>
 #include "LogicVariable.h"
+#include "Clause.h"
 using namespace std;
 
 class GAIndividual
 {
-public:
-	vector<LogicVariable> genotype;
+private:
+	vector<LogicVariable*>* genotype;
 
+public:
+	GAIndividual();
 	GAIndividual* mutation(float probability);
 	GAIndividual* crossOver(GAIndividual* otherParent);
+	float fitness(vector<Clause*>* clauses);
+
+private:
+	LogicVariable* getLogicVariableById(int id);
 
 };
 
