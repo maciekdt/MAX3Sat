a59d57d23553bb5cd130744ba3174ad282d31169
maciekdt <maciek.fi88@gmail.com>
2022-01-12 20:29:38 +0100

    Dodanie klas LogicVariable i modyfikacja Optymizer

--- GAOptymizer.h
+++ GAOptymizer.h
@@ -1,5 +1,18 @@
 #pragma once
-class GAOptymizer
-{
+#include<vector>
+#include "GAIndividual.h"
+using namespace std;
+
+class GAOptymizer{
+private:
+	int populationSize;
+	float crossoverProbabilty;
+	float mutationProbability;
+	vector<GAIndividual> solutions;
+
+public:
+	void setParameters(int populationSize, float crossoverProbabilty, float mutationProbability);
+	void initialize(vector<int> variables);
+
 };
 
---
+++ LogicVariable.h
@@ -0,0 +1,11 @@
+#pragma once
+class LogicVariable{
+public:
+	int id;
+	bool sign;
+	LogicVariable(int id, bool sign) {
+		this->id = id;
+		this->sign = sign;
+	}
+};
+
