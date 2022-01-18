8356a01e8e5004c5057586f8928498f804a02876
maciekdt <maciek.fi88@gmail.com>
2022-01-18 22:16:48 +0100

    xd

--- Main.cpp
+++ Main.cpp
@@ -16,10 +16,17 @@ int main() {
         printf("%s\n", pch);
         pch = strtok(NULL, " ,.-");
     }*/
+    
+    
+    Max3SatProblem* test = new Max3SatProblem();
+    test->load("m3s_50_0.txt");
     return 0;
+<<<<<<< HEAD
+=======
     */
     Max3SatProblem* test = new Max3SatProblem;
     test->load("m3s_50_0.txt");
     return 0;
+>>>>>>> 1e16e2222070ea4da98dfc99f742f91fa812e4b7
 
 }
